bc1c34441a3f0961755e5ec6484494407030dd6e
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-09-05 00:08:29 +0500

    Update super quality section to fix text bug

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -8,10 +8,10 @@ const SuperQuality = () => {
       className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container"
     >
       <div className="flex flex-1 flex-col">
-        <h2 className="mt-10 font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
-          We provide you&nbsp;
-          <span className="text-coral-red inline-block mt-3">Super&nbsp;</span>
-          <span className="text-coral-red inline-block mt-3">Quality</span>
+        <h2 className="font-palanquin capitalize text-4xl lg:max-w-lg font-bold">
+          We provide you
+          <span className="text-coral-red"> Super </span>
+          <span className="text-coral-red"> Quality </span>
         </h2>
         <p className="mt-4 lg:max-w-lg info-text">
           Ensuring premium comfort and style, our meticulously crafted footware
@@ -25,14 +25,14 @@ const SuperQuality = () => {
           <Button label="View Details" />
         </div>
       </div>
-      <div className="flex flex-1 justify-center items-center">
+      <div className="flex-1 flex justify-center items-center">
         <img
           src={shoe8}
           alt="shoe8"
           width={570}
           height={522}
           className="object-contain"
-        /> 
+        />
       </div>
     </section>
   );
