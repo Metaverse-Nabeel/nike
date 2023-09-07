7558f4dbaa76d4f56595325dd6463ca517166b5f
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-09-07 12:27:04 +0500

    Update contact information

--- src/constants/index.js
+++ src/constants/index.js
@@ -112,7 +112,7 @@ export const footerLinks = [
         title: "Get in touch",
         links: [
             { name: "customer@nike.com", link: "mailto:customer@nike.com" },
-            { name: "+92554862354", link: "tel:+92554862354" },
+            { name: "Engr. Nabeel Ahmed", link: "tel:++923215674724" },
         ],
     },
 ];
